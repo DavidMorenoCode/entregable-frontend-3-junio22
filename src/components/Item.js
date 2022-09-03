ffea5d66dcb87f74e44cb4f4f5c43a76bb91894c
David Moreno <davidmorenocode@gmail.com>
2022-09-03 07:56:47 -0500

    UPDATE (Item.js) Se añade la estructura

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,4 +1,8 @@
 
+import { useState } from "react"
+
+
+
 // El componente Item no tiene componentes hijos.
 // ESTADO: Item debe tener un número para almacenar la cantidad de stock, la misma se la defina el padre a la hora de crearlo.
 // MÉTODOS: Item debe manejar el click de su boton para restar la cantidad en stock de sí mismo y a su vez poder aumentar el estado de su "abuelo" App.
@@ -9,11 +13,25 @@
 //    h5 > span    (este span debe mostrar la cantidad si es mayor a 0 "agotado" si llega a 0)
 //    button       (este boton debe permitir comprar, pero si la cantidad es menor a 0 debe estar deshabilitado y decir "Sin stock")
 
-export default function Item() {
+export default function Item({titulo, descripcion, cantidad, agregarAlCarrito}) {
+
+  const [cantidadProducto, setcantidadProducto] = useState(cantidad)
+  const valDescontar = 1;
+  const  descontarCantidad   = () =>{
+    setcantidadProducto(cantidadProducto - valDescontar)
+  }
+  const spanAgotado = <span>agotado</span>
+  const spanCantidad = <span>{cantidadProducto}</span>
+
+  const buttonAgotado = <button disabled> Sin Stock</button>
+  const buttonCantidad = <button onClick={() => {descontarCantidad(); agregarAlCarrito();}}> Comprar</button>
 
   return (
     <div className='producto'>
-      {/* maquetar Item aquí */}
+      <h3>{titulo}</h3>
+      <p>{descripcion}</p>
+      <h5>En stock: {(cantidadProducto <= 0) ? spanAgotado : spanCantidad}</h5>
+      {( cantidadProducto <= 0 ) ? buttonAgotado : buttonCantidad}
     </div>
   )
 }
