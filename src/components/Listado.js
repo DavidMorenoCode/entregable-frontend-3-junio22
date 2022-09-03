b99c12009863655fbe50616f644f4d80bb231a03
David Moreno <davidmorenocode@gmail.com>
2022-09-03 08:15:52 -0500

    UPDATE (Listado.js) Se añade la estructura para el listado

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Item from './Item'
 
 // Debemos importar el listado de objetos del archivo JSON para usarlos como array a la hora de crear cada Item.
 // El componente Listado es el padre de:
@@ -7,10 +8,15 @@ import React from 'react'
 // MÉTODOS: Listado no requiere de métodos.
 // PROPS: Listado recibe el método para aumentar el estado de App y se lo pasa a cada uno de sus hijos.
 
-export default function Listado() {
+export default function Listado({agregarAlCarrito, data}) {
   return (
     <div className='container'>
-      {/* renderizamos los Item aquí */}
+      {
+        data.map(({id, stock, producto}) => {
+          const {nombre, descripcion } = producto;
+          return <Item key={id} titulo = {nombre} descripcion = {descripcion} cantidad = {stock} agregarAlCarrito = {agregarAlCarrito}></Item>
+        })
+      }
     </div>
   )
 }
